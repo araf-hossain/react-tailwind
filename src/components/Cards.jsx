6ed2a9a78029208fc4cdfbd072a9a31037804768
araf <arafnoob@gmail.com>
2023-06-10 16:02:59 +0600

    fix errors

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Lights from '../assets/city-lights.png';
+import ComingSoon from '../assets/coming-soon.png';
 
 const Cards = () => {
   return (
     <div className='w-full py-[10rem] px-4 bg-white'>
         <div className='max-w-[1230px] mx-auto grid md:grid-cols-3 gap-8'>
             <div className="w-full shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
-                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Lights} alt="" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={ComingSoon} alt="" />
                 <h3 className='text-2xl font-bold text-center py-8'>Single User</h3>
                 <p className='text-center text-4xl font-bold'>$149</p>
                 <div className='text-center font-medium'>
@@ -18,7 +18,7 @@ const Cards = () => {
             </div>
 
             <div className="w-full shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
-                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Lights} alt="" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={ComingSoon} alt="" />
                 <h3 className='text-2xl font-bold text-center py-8'>Single User</h3>
                 <p className='text-center text-4xl font-bold'>$209</p>
                 <div className='text-center font-medium'>
@@ -30,7 +30,7 @@ const Cards = () => {
             </div>
 
             <div className="w-full shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
-                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Lights} alt="" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={ComingSoon} alt="" />
                 <h3 className='text-2xl font-bold text-center py-8'>Single User</h3>
                 <p className='text-center text-4xl font-bold'>$249</p>
                 <div className='text-center font-medium'>
